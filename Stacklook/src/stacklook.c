443223b121521ae2993d3c2aa0b8db3177a0892d
David Jaromír Šebánek <djsebofficial@gmail.com>
2024-06-13 18:46:45 +0200

    Touched up CMakeLists + repurposing existing plugin codes

--- Stacklook/src/stacklook.c
+++ Stacklook/src/stacklook.c
@@ -3,20 +3,20 @@
 * Inner logic is in file 'Stacklook.cpp'
 */
 
+// C
 #include <stdc-predef.h>
 
 // KernelShark
 #include "libkshark.h"
 #include "libkshark-plot.h"
+#include "libkshark-plugin.h"
 
 // Plugin header
 #include "stacklook.h"
 
-// Just familiarizing myself, taken from the talk video
-
-#define FONT_SIZE 30
-const char* font_file = "/usr/share/fonts/truetype/FreeSans.ttf";
 struct ksplot_font font;
+char *font_file = NULL;
+int ss_event_id;
 
 /*Static hello world*/
 static struct ksplot_font* get_font() {
@@ -37,66 +37,48 @@ static void hello_world(struct kshark_cpp_argv* argv_c, int sd, int val, int dra
     ksplot_print_text(get_font(), &text_col, 300, 100, "Hello, World!");
 }
 
+static void sl_free_ctx(struct plugin_stacklook_ctx* sl_ctx)
+{
+	if (!sl_ctx)
+		return;
+
+	kshark_free_data_container(sl_ctx->stacks_data);
+}
+
+KS_DEFINE_PLUGIN_CONTEXT(struct plugin_stacklook_ctx, sl_free_ctx);
+
 /** Load the plugin **/
 int KSHARK_PLOT_PLUGIN_INITIALIZER(struct kshark_data_stream* stream) {
+    
+    struct plugin_stacklook_ctx* sl_ctx = __init(stream->stream_id);
+    if (!sl_ctx) {
+		__close(stream->stream_id);
+		return 0;
+	}
+
+    ss_event_id = kshark_find_event_id(stream, "sched/sched_switch");
+
+    if (!font_file)
+        font_file = ksplot_find_font_file("FreeSans", "FreeSans");
+    if (!font_file)
+        return 0;
+
     kshark_register_draw_handler(stream, hello_world);
     return 1;
 }
 
 /** Unload the plugin **/
 int KSHARK_PLOT_PLUGIN_DEINITIALIZER(struct kshark_data_stream* stream) {
-    kshark_unregister_draw_handler(stream, hello_world);
-    return 1;
-}
 
-/**
- * Don't mind me stealing text from public footage on the talk for future work :)
- * For me: Do look at the footage again to check the loot.
-*/
-// /** COPYRIGHT & so on... */
-// // C
-// #include <stdio.h>
-// // KernelShark examples
-// #include "priority_plot.h"
-// 
-// struct ksplot_font font;
-// char *font_file = NULL;
-// int ss_event_id;
-// 
-// struct ksplot_font *get_font_ptr() {
-//  if (!ksplot_font_is_loaded(&font))
-//      ksplot_init_font(&font, FONT_SIZE, font_file);
-//  return &font;
-// }
-// /** Plugin data instance. */
-// static struct kshark_data_container *plugin_data[KS_MAX_NUM_STREAMS] = (NULL);
-// /** Get the per Data stream plugin data. */
-// struct kshark_data_container *get_plugin_data(int sd)
-// {
-//  return plugin_data[sd];
-// }
-// 
-// static void plugin_process(struct kshark_data_stream *stream,
-// {
-//  int64_t next_prio;
-//  void *rec, struct kshark_entry *entry)
-//  if (kshark_read_record_field(stream, rec, "next_prio", &next_prio) == 0)
-//        kshark_data_container_append(plugin_data[stream->stream_id], entry, next_prio);
-// }
-// int KSHARK_PLOT_PLUGIN_INITIALIZER (struct kshark_data_stream *stream)
-// {
-// ss_event_id=kshark_find_event_id(stream, "sched/sched_switch");
-// if (!font_file)
-// font_file = ksplot_find_font_file("FreeSans", "FreeSans");
-// if (!font_file)
-// return 0;
-// plugin_data[stream->stream_id] = kshark_init_data_container();
-// kshark_register_event_handler(stream, ss_event_id, plugin_process); kshark_register_draw_handler(stream, draw_priority);
-// return 1;
-// }
-// /** Unload this plugin. */
-// int KSHARK PLOT_PLUGIN_DEINITIALIZER (struct kshark_data_stream *stream)
-// kshark_unregister_event_handler(stream, ss_event_id, plugin_process); kshark_unregister_draw_handler (stream, draw_priority);
-// kshark_free_data_container (plugin_data[stream->stream_id]); plugin_data[stream->stream_id] = NULL;
-// return 1;
-// }
+    struct plugin_stacklook_ctx* sl_ctx = __get_context(stream->stream_id);
+    int retval = 0;
+
+    if (sl_ctx) {
+        kshark_unregister_draw_handler(stream, hello_world);
+        retval = 1;
+    }
+
+    __close(stream->stream_id);
+
+    return retval;
+}
--- Stacklook/src/stacklook.h
+++ Stacklook/src/stacklook.h
@@ -1,9 +1,22 @@
 #ifndef _KS_PLUGIN_STACKLOOK_H
 #define _KS_PLUGIN_STACKLOOK_H
 
+#include "libkshark.h"
+#include "libkshark-plot.h"
+#include "libkshark-plugin.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+#define FONT_SIZE 30
+
+struct plugin_stacklook_ctx {
+    /** Data of stack-related events. **/
+    struct kshark_data_container* stacks_data;
+};
+
+KS_DECLARE_PLUGIN_CONTEXT_METHODS(struct plugin_stacklook_ctx)
 
 
 #ifdef __cplusplus
