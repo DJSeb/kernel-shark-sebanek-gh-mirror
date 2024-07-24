644b0c4fbbf287dd87799447591f27dfac4ac914
David Jaromír Šebánek <djsebofficial@gmail.com>
2024-07-24 18:49:13 +0200

    Utilities update

--- Stacklook/src/_utilities.hpp
+++ Stacklook/src/_utilities.hpp
@@ -5,9 +5,18 @@
 #include <cstdint>
 #include <string>
 #include <iostream>
- 
+
+/** @brief
+ * A basic counter for the logs, incremented on each log output.
+ */
 static uint32_t log_counter = 0;
 
+/**
+ * @brief Logging function for simple strings.
+ * 
+ * @param to_log: string to write on std::cout
+ * @param with_newline: whether to end the log with a newline
+*/
 void log(const std::string& to_log, bool with_newline = true) {
     std::cout << "  [LOG #" << log_counter << "] " << to_log;
     if (with_newline)
@@ -15,6 +24,15 @@ void log(const std::string& to_log, bool with_newline = true) {
     ++log_counter;
 }
 
+/**
+ * @brief Logging function for simple strings, which supports
+ * other value inputs as well.
+ * 
+ * @param to_log: string to write on std::cout
+ * @param other: any type that supports being written into a
+ * std::ostream
+ * @param with_newline: whether to end the log with a newline
+*/
 template<typename OtherInfoT>
 void log(const std::string& to_log, OtherInfoT other, bool with_newline = true) {
     std::cout << "  [LOG #" << log_counter << "] " << to_log;
