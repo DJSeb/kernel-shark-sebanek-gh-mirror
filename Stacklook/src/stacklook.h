f829caf674d865923683d03a62be55a39b483810
David Jaromír Šebánek <djsebofficial@gmail.com>
2024-08-30 09:30:30 +0200

    Added sched_wakeup preprocessing & changed sched_wakeup nap check function

--- Stacklook/src/stacklook.h
+++ Stacklook/src/stacklook.h
@@ -71,6 +71,12 @@ void draw_stacklook_objects(struct kshark_cpp_argv* argv_c, int sd,
 void* plugin_set_gui_ptr(void* gui_ptr);
 void clean_opened_views(void* view_container);
 void* init_views();
+#ifndef _NO_NAPS
+void wakeup_evt_tep_processing(struct plugin_stacklook_ctx* ctx, 
+                               struct kshark_data_stream* stream,
+                               void* rec,
+                               struct kshark_entry* entry);
+#endif
 
 #ifdef __cplusplus
 }
