eb0cef1a8aee9e1887c98154971c0cf5082db664
David Jaromír Šebánek <djsebofficial@gmail.com>
2025-04-20 19:58:27 +0200

    New plugin for ignoring taskboxes for some bins (experimental and frankly not good)

---
+++ NoBoxes/src/noboxes.c
@@ -0,0 +1,9 @@
+/** Copyright (C) 2025, David Jaromír Šebánek <djsebofficial@gmail.com> **/
+
+/**
+ * @file    noboxes.c
+ * @brief   
+*/
+
+// Plugin header
+#include "noboxes.h"
---
+++ NoBoxes/src/noboxes.h
@@ -0,0 +1,25 @@
+/** Copyright (C) 2025, David Jaromír Šebánek <djsebofficial@gmail.com> **/
+
+/**
+ * @file    noboxes.h
+ * @brief   
+ * 
+ * @note   
+*/
+
+#ifndef _KS_PLUGIN_NOBOXES_H
+#define _KS_PLUGIN_NOBOXES_H
+
+// KernelShark
+#include "libkshark.h"
+#include "libkshark-plugin.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // _KS_PLUGIN_NOBOXES_H
